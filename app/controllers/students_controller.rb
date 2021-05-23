3f8d6c77db7a2b743d8452013c067ea4da083f43
Reid <rlj0713@gmail.com>
2021-05-22 21:04:00 -0400

    commit tree cb6913f84e501cf2280e02b81f01b59f4040d397

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,19 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit([:first_name, :last_name])
   end
 
 end
